2da03aa6f2e1fde7ca99a978ae705aecb7eb2684
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-15 14:30:06 -0400

    done

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onEatSushi }) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+  const renderSushi = sushis
+  .slice(sushiIndex, sushiIndex +4)
+  .map((sushi)=> (
+    <Sushi key = {sushi.id} sushi = {sushi} onEatSushi = {onEatSushi}/>
+  ))
+  
+  function handleClickMore() {
+    setSushiIndex((sushiIndex)=> (sushiIndex+4) % sushis.length)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushi}
+      <MoreButton onClickMore={handleClickMore}/>
     </div>
   );
 }
